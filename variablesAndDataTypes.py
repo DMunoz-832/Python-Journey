e220676fccb0b5c0da13a0fd775a5e563f589755
Daniela Munoz <dmunoz9@hawk.iit.edu>
2023-06-28 00:30:05 -0500

    completion of 'variablesAndDataTypes' file. Includes: ints, True/False, variables.

---
+++ variablesAndDataTypes.py
@@ -0,0 +1,26 @@
+# The following text was provided from a random paragraph generator
+
+char_name = "Jimmy"
+story_light = "mark"
+
+print("The " + story_light + " blinded " + char_name + " .")
+print("It was dark and he thought he was alone, " + story_light + " shining in his eyes proved " + char_name + " wrong")
+print("He reached into his pocket and pulled out a flashlight stronger than the one blinding " + char_name + " .")
+
+print("-----------------")
+
+char_name = "Houston"
+char_age = int(20)
+is_male = True
+story_light = "light"
+
+print("The " + story_light + " blinded " + char_name + " .")
+print("It was dark and he thought he was alone, " + story_light + " shining in his eyes proved " + char_name + " wrong")
+print("He reached into his pocket and pulled out a flashlight stronger than the one blinding " + char_name + " .")
+print(char_name + " didn't make it, gone at the age of " + str(char_age) + " yrs old.")
+
+print("-----------------")
+
+test_numbers = int(2)
+
+print("This is a test to check int capabilities: " + str(test_numbers))
