4a2d1a2f36642c32588497676fc87812ce14333b
Daniela Munoz <dmunoz9@hawk.iit.edu>
2023-07-01 14:56:27 -0500

    completion of 'numbers' file. Includes: math import, max/min, abs, sqrt, etc.

---
+++ numbers.py
@@ -0,0 +1,39 @@
+from math import *
+
+# Pemdas
+print(32*2+4)
+
+# Modulus
+print(10%3)
+
+# String and Numbers
+my_num = 7
+print("$" + str(my_num) + " is how much money I have")
+
+# FUNCTIONS
+# Absolute Value (ABS)
+test_num = -42
+print(abs(test_num))
+
+# Power to - first number to the power of second number
+first_num = 2.4
+second_num = 5.1
+print(str(first_num) + " to the power of " + str(second_num) + " = " + str(pow(first_num, second_num)))
+
+# Max - returns the highest value of given values
+print("Max: " + str((max(597, 1038))))
+
+# Min - returns the smallest value of given values
+print("Min: " + str(min(-12, 0)))
+
+# Round - rounds values up or down
+print("Round: " + str(round(2.43)))
+
+# Floor - grabs the lowest value
+print("Floor " + str(floor(7.54)))
+
+# Ceil - grabs the highest value
+print("Ceil: " + str(ceil(3.97)))
+
+# Sqrt
+print("Sqrt: " + str(sqrt(81)))
